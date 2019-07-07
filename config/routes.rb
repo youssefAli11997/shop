2790f722da83939af3c612cd281efe4ce610c2c2
Youssef Ali <youssefali6212@gmail.com>
2019-07-07 21:08:28 +0200

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   get 'home/index'
   root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
